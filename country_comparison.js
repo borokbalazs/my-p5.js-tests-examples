d50618ec1e62fc48609e9003a2998d173885382d
borokbalazs <47794741+borokbalazs@users.noreply.github.com>
2019-04-25 11:14:26 +0200

    Update country_comparison.js

--- country_comparison.js
+++ country_comparison.js
@@ -24,11 +24,11 @@ let country={
 		height:0,
 		color:"#ff6464"
 	},
-	India:{
-		population:1399000000,
-		growthrate:0.11,
-		height:0,
-		color:"#f4e842"
+	USA:{
+	population:327200000,
+	growthrate:0.07,
+	height:0,
+	color:"#425cf4"
 	}
 
 }
@@ -37,10 +37,12 @@ let h = 10;
 var exc=0;
 var d = new Date();
 
+var popdiv = 1000000;
+
 let dat=d.getFullYear();
 
 function setup() {
-  createCanvas(1200,600);
+  createCanvas(1200,1200);
   for(let i = 0;i<Object.keys(country).length;i++){
   	if(i!=0){
       if(country[Object.keys(country)[i]].height<=country[Object.keys(country)[i-1]].height){
@@ -55,11 +57,14 @@ function draw(){
 	for(let i = 0;i<Object.keys(country).length;i++){
 		
 		fill(country[Object.keys(country)[i]].color);
-		rect(0,country[Object.keys(country)[i]].height,map(country[Object.keys(country)[i]].population/100000,0,width/2,0,width),50);
-		country[Object.keys(country)[i]].population += country[Object.keys(country)[i]].population * country[Object.keys(country)[i]].growthrate;
+		rect(0,country[Object.keys(country)[i]].height,country[Object.keys(country)[i]].population/popdiv,50);
+		country[Object.keys(country)[i]].population += country[Object.keys(country)[i]].population * country[Object.keys(country)[i]].growthrate/365;
 		fill(255);
 		textSize(32);
 		text(Object.keys(country)[i]+": "+ Math.round((country[Object.keys(country)[i]].population/1000000)*100)/100,0,country[Object.keys(country)[i]].height+35);
+		if(country[Object.keys(country)[i]].population/popdiv > width/2){
+			popdiv=popdiv*1.1;
+		}
 		}
 		fill(255);
 		textSize(32);
